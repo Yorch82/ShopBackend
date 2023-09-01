5cc9dbceecc9b6c7d6feea020be0179abe8ba958
Yorch82 <jcampo1982@outlook.com>
2023-09-01 18:28:40 +0200

    remove logplease

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-const Logger = require('logplease');
-const options = {
-  useColors: true, // Enable colors
-  color: Logger.Colors.Yellow, // Set the color of the logger
-  showTimestamp: true, // Display timestamp in the log message
-  useLocalTime: false, // Display timestamp in local timezone
-  showLevel: true, // Display log level in the log message
-  filename: null, // Set file path to log to a file
-  appendFile: true, // Append logfile instead of overwriting
-};
-const logger = Logger.create('Message', options);
 const express = require('express');
 const app = express();
 const port = 8000;
@@ -28,5 +17,5 @@ app.use('/reviews', require('./routes/reviews'));
 app.use('/sections', require('./routes/sections'));
 
 app.listen(port, () => {
-  logger.info('Server is running on ' + port);
+  console.log('Server is running on ' + port);
 });
